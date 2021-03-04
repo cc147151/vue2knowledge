209aadc87c0ab0e6d804c8e5d21fdf1fcc984fd6
yang <1471517131@qq.com>
2021-03-04 22:49:57 +0800

    fix:props1

--- src/views/assembly/components/Son.vue
+++ src/views/assembly/components/Son.vue
@@ -1,8 +1,9 @@
 <template>
   <div class="son">
     <van-button type="primary" @click="seeFun"
-      >点我查看父组件给我传递的函数名</van-button
+      >点我查看父组件给我传递的函数名{{$attrs.one}}</van-button
     >
+    <p>{{sonObj.name}}</p>
   </div>
 </template>
 <script>
@@ -11,17 +12,30 @@ import { Button } from "vant"
 Vue.use(Button)
 export default {
   data() {
-    return {}
+    return {
+      obj:{
+        name:'props无法读取到'
+      }
+    }
+  },
+  props: {
+    sonObj: {
+      type:Object,
+      default:function(){
+        console.log(this.obj)   // undefined
+       return {name:'未给子组件传值的时候显示的默认值(未传，既未在子组件上传递该属性,空值也视为传递了)，props里定义接受数据类型时，默认值无法读取到data定义的属性'}  
+      }
+    },
   },
   methods: {
     seeFun() {
       console.log(this._events)
-      console.log(this.$listeners,'listeners')
-      console.log(this.$attrs,'attrs')
-      this.$emit('son-fun','啊哈哈')
-      this.$emit('son-fun2',{name:"sss"})
-    }
-  }
+      console.log(this.$listeners, "listeners")
+      console.log(this.$attrs, "未在props注册的属性，可以接收到")
+      this.$emit("son-fun", "啊哈哈")
+      this.$emit("son-fun2", { name: "sss" })
+    },
+  },
 }
 </script>
 
--- src/views/assembly/index.vue
+++ src/views/assembly/index.vue
@@ -3,7 +3,11 @@
     <Nav />
     <p>
       1.在子组件的_events里，可以看到父组件传递给子组件监听的函数名
-      <Son @son-fun2="toSon2" attr="attr" :son-arr="sonData" />
+      <Son @son-fun2="toSon2"  attr="attr" :one="one"/>
+      <button @click="one=3">点我修改子组件值</button>
+    </p>
+    <p>
+      2.基础组件的自动化全局注册：main.js配置以后，无需再import引入，components注册
     </p>
   </div>
 </template>
@@ -12,7 +16,7 @@ import Son from "./components/Son"
 export default {
   data() {
     return {
-      sonData: []
+      one:1
     }
   },
   components: {
